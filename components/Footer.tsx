db3eba4399fcb6ced4252c9859d48a0a229bb41d
metoshiv@gmail.com <112664491+ShivHaxYT@users.noreply.github.com>
2024-08-29 13:24:50 -0700

    Update Footer to stick to the bottom.tsx
    Added Something LMAO first tweak ahh commit

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,10 +2,10 @@ import { Button } from "./ui/button";
 
 const Footer = () => {
   return (
-    <footer className="bg-slate-50 border-t-2 border-slate-200 h-32 flex justify-center">
+    <footer className="bg-slate-50 border-t-2 border-slate-200 h-32 flex justify-center fixed bottom-0 left-0 w-full">
       <div className="w-full items-center grid grid-cols-2 px-6 md:max-w-screen-2xl">
         <div>
-          <p>Copyright © 2024 Asher & Danny</p>
+          <p>Copyright © 2024 Asher & Danny & Shiv</p>
         </div>
         <div className="flex justify-end space-x-4">
           <Button variant="link">Terms of Service</Button>
