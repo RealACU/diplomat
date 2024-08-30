2450ab17cbc39c343cd3161925a7d7dab52c463e
RealACU <dannynnguyen7@gmail.com>
2024-08-30 08:52:44 -0700

    Forwarded Shiv's changes

--- app/(app)/page.tsx
+++ app/(app)/page.tsx
@@ -1,114 +1,59 @@
+"use client";
+
+import { useEffect, useRef } from "react";
 import Image from "next/image";
 
+const banner_texts = [
+  "Easy and fast tournament creation",
+  "Discover events in your area",
+  "Edit and share tournament information",
+  "Keep delegates updated through SMS and email",
+  "View and administer delegate results",
+  "Easy and fast tournament creation",
+  "Discover events in your area",
+  "Edit and share tournament information",
+  "Keep delegates updated through SMS and email",
+  "View and administer delegate results",
+  "Easy and fast tournament creation",
+  "Discover events in your area",
+  "Edit and share tournament information",
+  "Keep delegates updated through SMS and email",
+  "View and administer delegate results",
+];
+
 export default function Home() {
+  const textRef = useRef(null);
+
+  useEffect(() => {
+    const textElement = textRef.current;
+    if (textElement) {
+      // @ts-ignore
+      const content = textElement.innerHTML;
+      // @ts-ignore
+      textElement.innerHTML += content;
+    }
+  }, []);
+
   return (
     <div>
       <div className="relative h-screen">
         <div className="w-full h-screen bg-docblue-200 z-10 absolute opacity-50" />
         <div className="bg-frontpage w-full h-screen bg-no-repeat bg-center bg-cover absolute" />
 
-        <div className="relative whitespace-nowrap w-full z-0 mt-2 flex-nowrap inline-flex overflow-hidden">
-          <ul className="flex items-center justify-center text-lg text-white animate-infinite-scroll">
-            <li className="min-w-max flex items-center">
-              <Image
-                src="/star_white.png"
-                alt="star"
-                width={20}
-                height={0}
-                className="h-full mx-3"
-              />
-              Easy and fast tournament creation
-            </li>
-            <li className="min-w-max flex items-center">
-              <Image
-                src="/star_white.png"
-                alt="star"
-                width={20}
-                height={0}
-                className="h-full mx-3"
-              />
-              Discover events in your area
-            </li>
-            <li className="min-w-max flex items-center">
-              <Image
-                src="/star_white.png"
-                alt="star"
-                width={20}
-                height={0}
-                className="h-full mx-3"
-              />
-              Edit and share tournament information
-            </li>
-            <li className="min-w-max flex items-center">
-              <Image
-                src="/star_white.png"
-                alt="star"
-                width={20}
-                height={0}
-                className="h-full mx-3"
-              />
-              Keep delegates updated through SMS and email
-            </li>
-            <li className="min-w-max flex items-center">
-              <Image
-                src="/star_white.png"
-                alt="star"
-                width={20}
-                height={0}
-                className="h-full mx-3"
-              />
-              View and administer delegate results
-            </li>
-            <li className="min-w-max flex items-center">
-              <Image
-                src="/star_white.png"
-                alt="star"
-                width={20}
-                height={0}
-                className="h-full mx-3"
-              />
-              Easy and fast tournament creation
-            </li>
-            <li className="min-w-max flex items-center">
-              <Image
-                src="/star_white.png"
-                alt="star"
-                width={20}
-                height={0}
-                className="h-full mx-3"
-              />
-              Discover events in your area
-            </li>
-            <li className="min-w-max flex items-center">
-              <Image
-                src="/star_white.png"
-                alt="star"
-                width={20}
-                height={0}
-                className="h-full mx-3"
-              />
-              Edit and share tournament information
-            </li>
-            <li className="min-w-max flex items-center">
-              <Image
-                src="/star_white.png"
-                alt="star"
-                width={20}
-                height={0}
-                className="h-full mx-3"
-              />
-              Keep delegates updated through SMS and email
-            </li>
-            <li className="min-w-max flex items-center">
-              <Image
-                src="/star_white.png"
-                alt="star"
-                width={20}
-                height={0}
-                className="h-full mx-3"
-              />
-              View and administer delegate results
-            </li>
+        <div className="relative banner-container">
+          <ul className="banner-text text-lg text-white" ref={textRef}>
+            {banner_texts.map((banner_text, index) => (
+              <li key={index} className="min-w-max flex items-center">
+                <Image
+                  src="/star_white.png"
+                  alt="star"
+                  width={20}
+                  height={20}
+                  className="h-full mx-3"
+                />
+                {banner_text}
+              </li>
+            ))}
           </ul>
         </div>
 
@@ -142,15 +87,15 @@ export default function Home() {
                 src="/search_icon.png"
                 alt="search_icon"
                 width={30}
-                height={0}
+                height={30}
                 className="h-auto mx-4"
-              ></Image>
+              />
               <input
                 type="text"
                 id="search-bar"
                 placeholder="Search for a tournament near you"
                 className="text-xl font-semibold text-slate-800 placeholder-slate-300"
-              ></input>
+              />
             </div>
             <div className="w-full h-[465px]"></div>
           </div>
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,11 +1,40 @@
+"use client";
+
+import { useEffect, useState } from "react";
 import { Button } from "./ui/button";
 
 const Footer = () => {
+  const [visible, setVisible] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollPosition = window.scrollY + window.innerHeight;
+      const triggerPosition = document.body.offsetHeight * 0.7;
+      const nearTop = window.scrollY < 50;
+
+      if (scrollPosition >= triggerPosition && !nearTop) {
+        return setVisible(true);
+      }
+
+      setVisible(false);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <footer className="bg-slate-50 border-t-2 border-slate-200 h-32 flex justify-center fixed bottom-0 left-0 w-full">
+    <footer
+      className={`bg-slate-50 border-t-2 border-slate-200 h-32 flex justify-center fixed left-0 w-full transition-all duration-500 ${
+        visible ? "bottom-0" : "-bottom-32"
+      }`}
+    >
       <div className="w-full items-center grid grid-cols-2 px-6 md:max-w-screen-2xl">
         <div>
-          <p>Copyright © 2024 Asher & Danny & Shiv</p>
+          <p>Copyright © 2024 Asher & Danny & Shivansh</p>
         </div>
         <div className="flex justify-end space-x-4">
           <Button variant="link">Terms of Service</Button>
