67d1d354a690ce683c97b272c6a8c38d5c08445c
akay888 <ashkay1011@gmail.com>
2024-10-14 06:50:18 -0700

    fixed navbar again😭😭❓

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -62,30 +62,30 @@ const Navbar = async () => {
               </Link>
             </Button>
           </SignedIn>
+
+          <SignedIn>
+            <div className="h-auto w-auto flex items-center justify-center ml-2">
+              <UserButton />
+            </div>
+          </SignedIn>
+
+          <SignedOut>
+            <div className="hidden sm:flex gap-5">
+              <Button
+                asChild
+                className="text-[16px] bg-gradient-to-b from-slate-400 to-[#7388a2] py-2 px-6 rounded-md hover:bg-gradient-to-bl hover:scale-105 transition duration-100 flex-shrink-0 font-semibold"
+              >
+                <Link href="/sign-in">Sign in</Link>
+              </Button>
+              <Button
+                asChild
+                className="text-[16px] bg-gradient-to-b from-[#c1ad5e] to-[#d6975f] py-2 px-10 rounded-md hover:bg-gradient-to-br hover:scale-105 transition duration-100 flex-shrink-0"
+              >
+                <Link href="/sign-up">Get started</Link>
+              </Button>
+            </div>
+          </SignedOut>
         </div>
-
-        <SignedIn>
-          <div className="h-auto w-auto flex items-center justify-center ml-8">
-            <UserButton />
-          </div>
-        </SignedIn>
-
-        <SignedOut>
-          <div className="hidden sm:flex gap-5">
-            <Button
-              asChild
-              className="text-[16px] bg-gradient-to-b from-slate-400 to-[#7388a2] py-2 px-6 rounded-md hover:bg-gradient-to-bl hover:scale-105 transition duration-100 flex-shrink-0 font-semibold"
-            >
-              <Link href="/sign-in">Sign in</Link>
-            </Button>
-            <Button
-              asChild
-              className="text-[16px] bg-gradient-to-b from-[#c1ad5e] to-[#d6975f] py-2 px-10 rounded-md hover:bg-gradient-to-br hover:scale-105 transition duration-100 flex-shrink-0"
-            >
-              <Link href="/sign-up">Get started</Link>
-            </Button>
-          </div>
-        </SignedOut>
       </div>
     </nav>
   );
