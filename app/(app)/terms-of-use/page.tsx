a4b5ea6c80d74cdb1a7202b5e8022b4493e4fc2d
RealACU <dannynnguyen7@gmail.com>
2024-10-21 17:59:11 -0700

    fix

--- app/(app)/terms-of-use/page.tsx
+++ app/(app)/terms-of-use/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useState } from "react";
-import "./termsOfUse.css";
 
 const TermsOfUsePage = () => {
   const [highlightedSection, setHighlightedSection] = useState<string | null>(
